53f661712f15731946be6fa4474fa560d792b4ac
nidhintony <nidhintony@gmail.com>
2016-03-27 20:52:20 +0530

    Initial commit.

---
+++ src/main/java/com/vw/address/AddressApiApplication.java
@@ -0,0 +1,12 @@
+package com.vw.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vw/address/AddressApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vw.address;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AddressApiApplication.class)
+public class AddressApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
